771f9359fa50386fcd08b00abadf16e5407e007e
liujiexian <361684727@qq.com>
2020-04-30 09:49:40 +0000

    U: 基准修复

--- app/service/backtestresult.ts
+++ app/service/backtestresult.ts
@@ -106,6 +106,21 @@ export default class BacktestResult extends Service {
 
   public async Standard(data: any) {
     try {
+      let Interval = data.Interval;
+      if (data.TradeType === 'Forex') {
+        // 1:1m 2:5m 3:15m 4:30m 5:1h
+        if (data.Interval === '1m') {
+          Interval = 60;
+        } else if (data.Interval === '5m') {
+          Interval = 300;
+        } else if (data.Interval === '15m') {
+          Interval = 900;
+        } else if (data.Interval === '30m') {
+          Interval = 1800;
+        } else if (data.Interval === '30m') {
+          Interval = 3600;
+        }
+      }
       const DatadetailsList = await this.ctx.model.Datadetails.findAll({
         where: {
           Exchange: data.Exchange || '',
@@ -113,67 +128,79 @@ export default class BacktestResult extends Service {
           Symbol: data.Symbol,
           DataType: data.DataType,
           TradeType: data.TradeType,
-          Interval: data.Interval,
+          Interval,
         },
         raw: true,
       });
+      console.log(DatadetailsList[0]);
+      if (DatadetailsList.length > 0) {
+        const DPIDArr = DatadetailsList.map(DataPacket => {
+          return DataPacket.DataPacketId;
+        });
 
-      const DPIDArr = DatadetailsList.map(DataPacket => {
-        return DataPacket.DataPacketId;
-      });
-
-      const res = await this.ctx.service.backtestresult.StrategyOrderList(data.BacktestId);
-      const StrategyOrderList = res.Result.StrategyOrderList;
-      let FirstFillPrice = 0;
-      if (StrategyOrderList.length) {
-        const list: any = StrategyOrderList[0];
-        FirstFillPrice = list.FillPrice;
-      }
-      const timeArr: any = [];
-      const filltimeArr: any = [];
-      const Result: any = {};
-      const OrderResult: any = {};
-      StrategyOrderList.forEach((item: any) => {
-        const filltime = moment.tz(item.FillTime, 'UTC').format('YYYY-MM-DD HH:mm:ss');
-        const time = moment.tz(item.FillTime, 'UTC').format('YYYY-MM-DD HH:mm:00 Z');
-        timeArr.push(`'${time}'`);
-        filltimeArr.push(filltime);
-      });
-      const timeStr = timeArr.toString();
-      const { QueryTypes } = this.app.Sequelize;
-      const Datapacket: any = await this.app.pgmodel.query(
-        `select * from ${DPIDArr[0]} where date_trunc('min', time) in (${timeStr})`,
-        { type: QueryTypes.SELECT },
-      );
-      Datapacket.forEach(data => {
-        let time = moment.tz(data.time, 'UTC').format('YYYY-MM-DD HH:mm:00');
-        time = time.toString();
-        Result[time] = data.close;
-      });
-      let OldValue = 0;
-      filltimeArr.forEach(item => {
-        const filltime = item.toString();
-        let time = moment.tz(item, 'UTC').format('YYYY-MM-DD HH:mm:00');
-        time = time.toString();
-        if (Result[time]) {
-          OldValue = Result[time];
+        const res = await this.ctx.service.backtestresult.StrategyOrderList(data.BacktestId);
+        const StrategyOrderList = res.Result.StrategyOrderList;
+        let FirstFillPrice = 0;
+        if (StrategyOrderList.length) {
+          const list: any = StrategyOrderList[0];
+          FirstFillPrice = list.FillPrice;
         }
-        OrderResult[filltime] = ((OldValue - FirstFillPrice) / data.Amount).toFixed(4);
-      });
-      let Standart: any = [];
-      Object.keys(OrderResult).forEach(function(key) {
-        const time = parseFloat(moment.tz(key, 'UTC').format('x'));
-        const value = parseFloat(OrderResult[key]);
-        Standart.push([ time, value ]);
-      });
-      Standart = Standart.sort(function(val1, val2) {
-        return val1[0] - val2[0];
-      });
+        const timeArr: any = [];
+        const filltimeArr: any = [];
+        const Result: any = {};
+        const OrderResult: any = {};
+        StrategyOrderList.forEach((item: any) => {
+          const filltime = moment.tz(item.FillTime, 'UTC').format('YYYY-MM-DD HH:mm:ss');
+          const time = moment.tz(item.FillTime, 'UTC').format('YYYY-MM-DD HH:mm:00 Z');
+          timeArr.push(`'${time}'`);
+          filltimeArr.push(filltime);
+        });
+        const timeStr = timeArr.toString();
+        const { QueryTypes } = this.app.Sequelize;
+        const Datapacket: any = await this.app.pgmodel.query(
+          `select * from ${DPIDArr[0]} where date_trunc('min', time) in (${timeStr})`,
+          { type: QueryTypes.SELECT },
+        );
+        console.log(Datapacket);
+        if (Datapacket.length > 0) {
+          Datapacket.forEach(data => {
+            let time = moment.tz(data.time, 'UTC').format('YYYY-MM-DD HH:mm:00');
+            time = time.toString();
+            Result[time] = data.close;
+          });
+          console.log(DPIDArr, Datapacket);
+          let OldValue = 0;
+          filltimeArr.forEach(item => {
+            const filltime = item.toString();
+            let time = moment.tz(item, 'UTC').format('YYYY-MM-DD HH:mm:00');
+            time = time.toString();
+            if (Result[time]) {
+              OldValue = Result[time];
+            }
+            OrderResult[filltime] = ((OldValue - FirstFillPrice) / data.Amount).toFixed(4);
+          });
+          let Standart: any = [];
+          Object.keys(OrderResult).forEach(function(key) {
+            const time = parseFloat(moment.tz(key, 'UTC').format('x'));
+            const value = parseFloat(OrderResult[key]);
+            Standart.push([ time, value ]);
+          });
+          Standart = Standart.sort(function(val1, val2) {
+            return val1[0] - val2[0];
+          });
+          return {
+            Head: {
+              Code: '200', Message: '获取列表成功！', CallTime: moment().tz('UTC').format('YYYYMMDDHHmmss'),
+            },
+            Result: Standart,
+          };
+        }
+      }
       return {
         Head: {
-          Code: '200', Message: '获取列表成功！', CallTime: moment().tz('UTC').format('YYYYMMDDHHmmss'),
+          Code: '500', Message: '基准数据包无数据/不存在，请同步数据库', CallTime: moment().tz('UTC').format('YYYYMMDDHHmmss'),
         },
-        Result: Standart,
+        Result: [],
       };
     } catch (error) {
       throw new Error(error.message);
