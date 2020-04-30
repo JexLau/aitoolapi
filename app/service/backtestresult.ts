d2c8a46c70d074293ca8467f6837d91402d47fc0
liujiexian <361684727@qq.com>
2020-04-30 12:08:52 +0800

    F: 修复total

--- app/service/backtestresult.ts
+++ app/service/backtestresult.ts
@@ -26,6 +26,9 @@ export default class BacktestResult extends Service {
       });
     }
     const [ StrategyorderCount ] = await this.ctx.model.Strategyorder.findAll({
+      attributes: [
+        [ this.app.Sequelize.fn('COUNT', this.app.Sequelize.col('Id')), 'total' ],
+      ],
       where: {
         JobId: BacktestId,
       },
@@ -65,6 +68,9 @@ export default class BacktestResult extends Service {
       });
     }
     const [ LogsListCount ] = await this.ctx.model.Logs.findAll({
+      attributes: [
+        [ this.app.Sequelize.fn('COUNT', this.app.Sequelize.col('Id')), 'total' ],
+      ],
       where: {
         BacktestId,
       },
